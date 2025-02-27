1b3dde9dbb0639c41360e587e97925addade4cb8
JuanCa-16 <juancamilohg162003@gmail.com>
2025-02-27 18:31:58 -0500

    CRUD for grados from the admin user

--- src/Contexts/UserContext.jsx
+++ src/Contexts/UserContext.jsx
@@ -1,9 +1,14 @@
 import { createContext, useContext, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+
+    const [user, setUser] = useState(()=>{
+        const token = localStorage.getItem("token");
+        return token ? jwtDecode(token) : null;
+    });
     return (
         <UserContext.Provider value={{ user, setUser }}>
             {children}
