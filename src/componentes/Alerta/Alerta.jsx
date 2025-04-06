94cf0123a1d8924fec9e997ed3441292c152bdda
JC1602 <jlondononeira@gmail.com>
2025-04-06 14:46:17 -0500

    fix arlert

--- src/componentes/Alerta/Alerta.jsx
+++ src/componentes/Alerta/Alerta.jsx
@@ -2,14 +2,21 @@ import { toast } from 'react-hot-toast';
 import './Alerta.scss';
 
 const Alerta = {
-    success: (message) =>
-    toast.success(`${message}`, { className: "my-toast my-toast-success" }),
-    error: (message) =>
-    toast.error(`${message}`, { className: "my-toast my-toast-error" }),
+    success: (message, grande = false) =>
+        toast.success(`${message}`, {
+            className: `my-toast my-toast-success ${grande ? 'my-toast-large' : ''}`
+        }),
+
+    error: (message, grande = false) =>
+        toast.error(`${message}`, {
+            className: `my-toast my-toast-error ${grande ? 'my-toast-large' : ''}`
+        }),
+
     info: (message) =>
-    toast(`${message}`, { className: "my-toast my-toast-info" }),
+        toast(`${message}`, { className: "my-toast my-toast-info" }),
+
     custom: (message, options) =>
-    toast(`${message}`, { className: "my-toast", ...options }),
+        toast(`${message}`, { className: "my-toast", ...options }),
 };
 
 export default Alerta;
