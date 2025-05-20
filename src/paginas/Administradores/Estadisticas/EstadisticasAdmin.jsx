2330adb1497182de74ccf24b260c9c249cdb31ca
Isabella <isabella.rebellon@correounivalle.edu.co>
2025-05-20 17:38:35 -0500

    the statistics pdf was produced

--- src/paginas/Administradores/Estadisticas/EstadisticasAdmin.jsx
+++ src/paginas/Administradores/Estadisticas/EstadisticasAdmin.jsx
@@ -7,7 +7,7 @@ const EstadisticasAdmin = () => {
 	return (
 		<div className='contenedorEstAdmin'>
 			<TituloDes
-				titulo='ESTADÍSTICAS DE LAINSTITUCIÓN'
+				titulo='ESTADÍSTICAS DE LA INSTITUCIÓN'
 				desc='Consulta estadísticas de tu institución para obtener información detallada sobre el rendimiento académico y otros aspectos clave para tomar mejores decisiones.'
 			></TituloDes>
 			<AdminEstadisticas></AdminEstadisticas>
