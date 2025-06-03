310f5b6102129fcf9fb9d7f2583321dc22748c04
JuanCa-16 <juancamilohg162003@gmail.com>
2025-06-03 11:26:44 -0500

    new back url

--- src/componentes/Estadisticas/WebSocketListener/Socket.jsx
+++ src/componentes/Estadisticas/WebSocketListener/Socket.jsx
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 //conexion url back con wss y sin /
-const socket = io('wss://mi-backend-prae-55793889802.us-central1.run.app', {
+const socket = io('wss://prae-backend-55793889802.us-south1.run.app', {
 	transports: ['websocket', 'polling'],
 	reconnection: true,
 	reconnectionAttempts: 5,
